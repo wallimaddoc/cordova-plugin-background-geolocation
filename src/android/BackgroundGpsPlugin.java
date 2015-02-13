88b5d674da510cfd94399199a952295546d81e77
Dr. Erik Wallacher <wallacher@innomis.com>
2015-02-13 08:30:14 +0100

    Bug fix for android BackgroundGpsPlugin

--- src/android/BackgroundGpsPlugin.java
+++ src/android/BackgroundGpsPlugin.java
@@ -2,6 +2,7 @@ package com.tenforwardconsulting.cordova.bgloc;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
